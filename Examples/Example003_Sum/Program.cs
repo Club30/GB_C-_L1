0ec8d431519cd2aadfd5368d96ed78ee2e76fc53
club30 <rusremont78@gmail.com>
2022-08-18 19:40:44 +0300

    add Example 003 Sum and Example 004 Div

---
+++ Examples/Example003_Sum/Program.cs
@@ -0,0 +1,4 @@
+﻿double numberA = 3;
+double numberB = 8;
+double sum = numberA + numberB;
+Console.WriteLine(sum);
---
+++ Examples/Example004_Div/Program.cs
@@ -0,0 +1,4 @@
+﻿double numberA = 3;
+double numberB = 8;
+double div = numberA / numberB;
+Console.WriteLine(div);
